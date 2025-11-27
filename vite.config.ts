87b395e704c69c93f5a107bee2136233806ad596
voiceh0924-blip <voice.h0924@gmail.com>
2025-11-27 11:18:27 +0900

    Update vite.config.ts
    Signed-off-by: voiceh0924-blip <voice.h0924@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -25,4 +24,3 @@ export default defineConfig({
     },
   },
 })
-```
